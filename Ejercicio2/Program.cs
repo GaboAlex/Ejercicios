7d4dc9cfa162b941bfd44ab87958afdf86f4c63a
GaboAlex <44093861+GaboAlex@users.noreply.github.com>
2018-10-23 10:29:05 -0500

    Actualizacion del Ejercicio 2
    Se termino el Ejercicio 2 y se esta subiendo

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            string numero = Console.ReadLine();
+            int x = 0;
+            bool logrado = Int32.TryParse(numero, out x);
+            if (logrado)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine("Es Par");
+                }
+                else
+                {
+                    Console.WriteLine("Es ImPar");
+                }
+            }
+            else {
+                Console.WriteLine("Ingrese solo numeros");
+            }
+        }
+    }
+}
