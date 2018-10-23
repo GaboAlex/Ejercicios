390caa2c3aee0b0a22985680c8f4870ddf9cb1d0
GaboAlex <44093861+GaboAlex@users.noreply.github.com>
2018-10-23 14:11:32 -0500

    Ejercicio 11
    Termino del trabajo con el ejercicio 1 hasta el 11

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer una calculadora que pida un operación(suma, resta, multiplicación, división y potencia) 
+            //y luego 2 números para resolver la operación.
+            Console.WriteLine("*************CALCULADORA***************\n");
+            
+
+            Console.WriteLine("\n****Elige una opcion ****\n" +
+                "\n1.- Suma" +
+                "\n2.- Resta" +
+                "\n3.- Multiplicar" +
+                "\n4.- Division" +
+                "\n5.- Potencia \n");
+            String s1 = null;
+            s1 = Console.ReadLine();
+
+            Console.WriteLine("Ingrese Primer numero: ");
+            int n1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Segundo numero: ");
+            int n2 = Int32.Parse(Console.ReadLine());
+
+            switch (s1)
+            {
+                case "1":
+                    int suma = n1 + n2;
+                    Console.WriteLine($"El resultado es: {suma}");
+                    break;
+                case "2":
+                    int resta = n1 - n2;
+                    Console.WriteLine($"El resultado es: {resta}");
+                    break;
+                case "3":
+                    int multiplicacion = n1 * n2;
+                    Console.WriteLine($"El resultado es: {multiplicacion}");
+                    break;
+                case "4":
+                    int division = n1 / n2;
+                    Console.WriteLine($"El resultado es: {division}");
+                    break;
+                case "5":
+                    int potencia = n1 ^ n2;
+                    Console.WriteLine($"El resultado es: {potencia}");
+                    break;
+                default:
+                    Console.WriteLine("Ingreso incorrecto");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Sueldo = 50000;
+            Console.WriteLine("Ingrese el numero de años Trabajados: ");
+            int n1 = Int32.Parse(Console.ReadLine());
+            double total = 0;
+            if (n1 >= 10)
+            {
+                total = Sueldo + (Sueldo * 0.1);
+            }
+            else if (n1 < 10 && n1 >= 5)
+            {
+                total = Sueldo + (Sueldo * 0.07);
+            }
+            else if (n1 < 5 && n1 >= 3)
+            {
+                total = Sueldo + (Sueldo * 0.05);
+            }
+            else {
+                total = Sueldo + (Sueldo * 0.03);
+            }
+            Console.WriteLine("El sueldo que recibira sera de: " + total);
+        }
+    }
+}
